556e06e104241c7b08ca256080a532e18bc628fe
gaobaoru-lenovo <gaobaoru2010@126.com>
2016-08-23 15:31:49 +0800

    create worker thread

---
+++ socket_examples/worker_thread/worker_thread.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <pthread.h>
+void* thread_summation(void *arg);
+int sum = 0;
+
+int main(int argc, char *argv[]){
+    pthread_t t_id1,t_id2;
+    int range1[] = {1, 5};
+    int range2[] = {6, 10};
+
+    pthread_create(&t_id1, NULL, thread_summation, (void*)range1);
+    pthread_create(&t_id2, NULL, thread_summation, (void*)range2);
+    
+    pthread_join(t_id1, NULL);
+    pthread_join(t_id2, NULL);
+    printf("result : %d \n", sum);
+    return 0;
+}
+
+void* thread_summation(void *arg){
+    int start = ((int*)arg)[0];
+    int end = ((int*)arg)[1];
+    while(start <= end){
+	sum += start;
+	start++;
+    }
+    return NULL;
+}
