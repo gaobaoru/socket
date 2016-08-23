688769bafa0e3fd3108efa5870de7d171f1be4d5
gaobaoru-lenovo <gaobaoru2010@126.com>
2016-08-23 16:26:01 +0800

    solve the problems about multiple based on mutex

---
+++ socket_examples/mutex/mutex.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#define THREAD_NUM 100
+void* thread_inc(void* arg);
+void* thread_des(void* arg);
+long long num = 0;
+pthread_mutex_t mutex;
+
+int main(int argc, char *argv[]){
+    pthread_t thread_id[THREAD_NUM];
+    int i;
+
+    pthread_mutex_init(&mutex, NULL);    
+
+    for(i = 0; i < THREAD_NUM; i++){
+	if(i%2)
+	    pthread_create(&(thread_id[i]), NULL, thread_inc, NULL);
+	else
+	    pthread_create(&(thread_id[i]), NULL, thread_des, NULL);
+    }
+
+    for(i = 0; i < THREAD_NUM; i++){
+	pthread_join(thread_id[i], NULL);
+    }
+    printf("result : %lld \n", num);
+    pthread_mutex_destroy(&mutex);
+    return 0;
+}
+
+void* thread_inc(void* arg){
+    int i;
+    pthread_mutex_lock(&mutex);
+    for(i = 0; i < 500000; i++)
+   	num += 1;
+    pthread_mutex_unlock(&mutex);
+    return NULL;
+}
+
+void* thread_des(void* arg){
+    int i;
+    for(i = 0; i < 500000; i++){
+	pthread_mutex_lock(&mutex); 
+	num -= 1;
+	pthread_mutex_unlock(&mutex);
+    }
+    return NULL;
+}
